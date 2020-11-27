49840c95bc8de0b08c93f05b97afca0d45528aa0
Nathan Davis <nathan@nathan.salesrabbit>
2020-11-27 13:32:06 -0700

    updates for flags

--- test/mock_me_phoenix_example/services/star_wars_test.exs
+++ test/mock_me_phoenix_example/services/star_wars_test.exs
@@ -4,26 +4,26 @@ defmodule MockMePhoenixExample.Services.StarWarsTest do
 
   # setup_all %{} do
   #   # re-initializes the test case state
-  #   MockMe.reset_test_cases()
+  #   MockMe.reset_flags()
   # end
 
   test "people/1 returns success" do
-    MockMe.set_test_case(:swapi_people, :success)
+    MockMe.set_flag(:swapi_people, :success)
     assert {:ok, _} = StarWars.people(1)
   end
 
   test "people/1 returns not found" do
-    MockMe.set_test_case(:swapi_people, :not_found)
+    MockMe.set_flag(:swapi_people, :not_found)
     assert {:not_found, _} = StarWars.people(1)
   end
 
   test "starships/1 returns success" do
-    MockMe.set_test_case(:swapi_starships, :success)
+    MockMe.set_flag(:swapi_starships, :success)
     assert {:ok, _} = StarWars.starships(1)
   end
 
   test "starships/1 returns not found" do
-    MockMe.set_test_case(:swapi_starships, :not_found)
+    MockMe.set_flag(:swapi_starships, :not_found)
     assert {:not_found, _} = StarWars.starships(1)
   end
 end
